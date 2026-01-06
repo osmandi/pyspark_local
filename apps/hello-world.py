776ca81211ac693a4d0d4605ca095c6a5a5539b0
Osmandi <osmandigt@gmail.com>
2026-01-06 07:33:01 -0500

    Rename the app name

--- apps/hello-world.py
+++ apps/hello-world.py
@@ -1,7 +1,10 @@
 from pyspark.sql import SparkSession
+from os.path import basename
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Spark cluster mode 2").getOrCreate()
+    filename = basename(__file__)
+    print(filename)
+    spark = SparkSession.builder.appName(filename).getOrCreate()
     try:
         print(f"Spark version: {spark.version}")
         df = spark.createDataFrame([(1,)], ["column_name"])
